00866b678f4f22a8fe42d1132077e33be90005ba
S.Zelensky <zelenstein@gmail.com>
2025-09-10 16:44:17 +0300

    title

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,9 @@
 import mobileNav from './modules/mobile-nav.js';
 import gridGuide from './modules/grid-guide.js';
-import slider from './modules/slider.js';
 // import accordeon from './modules/accordeon.js';
 
 document.addEventListener('DOMContentLoaded', () => {
   mobileNav();
   gridGuide();
-  slider();
   // accordeon();
 });
